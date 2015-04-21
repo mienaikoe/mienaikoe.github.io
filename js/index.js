10f6576b8baf1c944630346cb9b1922bcc066616
Jesse Lee <mienaikoe@gmail.com>
2015-04-21 02:51:58 -0400

    adding FirstHand

--- js/index.js
+++ js/index.js
@@ -64,7 +64,7 @@ var skillSets = [
 		title: "Frameworks",
 		entries: [
 			{ name: "Ruby on Rails", rating: 4 },
-			{ name: "Node.js", rating: 3 },
+			{ name: "Node/Express", rating: 3 },
 			{ name: "Google App Engine", rating: 2 },
 			{ name: "Spring", rating: 1 },
 			{ name: "Angular.js", rating: 1 }
@@ -75,10 +75,10 @@ var skillSets = [
 		title: "Tools",
 		entries: [
 			{ name: "JQuery", rating: 4 },
+			{ name: "Web Audio", rating: 4 },
+			{ name: "React", rating: 3 },
 			{ name: "Backbone.js", rating: 3 },
-			{ name: "Web Audio", rating: 3 },
-			{ name: "Three.js", rating: 3 },
-			{ name: "React", rating: 2 },
+			{ name: "Three.js", rating: 2 },
 			{ name: "HTML5 Canvas", rating: 2 },
 			{ name: "Apache Web Server", rating: 2 }
 		]
