c510b37be79a11b2e9de1c2a4beeffb8c0d665e1
Jesse <jesse@kinvite.co>
2018-08-09 14:22:17 -0400

    better code for print

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
 	var stuckHeader = null;
 	var stickyHeader = $("#stickyHeader");
 	var stickyPaper = stickyHeader.find(".paper");
-	
+
 	$(document).scroll(function(ev){
 		var scrollTop = $(window).scrollTop();
 		var bestHeader = null;
@@ -50,95 +50,63 @@ $(document).ready(function(){
 
 var skillSets = [
 	{
-		title: "Languages",
+		title: "Hardware Domain",
 		entries: [
-			{ name: "JavaScript", rating: 5 },
-			{ name: "Java", rating: 3 },
-			{ name: "Scala", rating: 3 },
-			{ name: "Ruby", rating: 2 },
-			{ name: "Python", rating: 2 },
-			{ name: "PHP", rating: 2 }
-		]
-	},
-	
-	{
-		title: "Frameworks",
-		entries: [
-			{ name: "React", rating: 4 },
-			{ name: "React Native", rating: 4 },
-			{ name: "Node/Express", rating: 4 },
-			{ name: "Play Framework", rating: 3 },
-			{ name: "Ruby on Rails", rating: 2 },
-		]
-	},
-	{
-		title: "DB & Devops",
-		entries: [
-			{ name: "PostgreSQL", rating: 4 },
-			{ name: "Google Cloud", rating: 3 },
-			{ name: "Redis", rating: 3 },
-			{ name: "Docker/Kubernetes", rating: 2 },
-			{ name: "Load Balancing/CDNs", rating: 2 },
-		]
-	},
-	{
-		title: "CS Topics",
-		entries: [
-			{ name: "Geo & Maps", rating: 4 },
-			{ name: "Audio & Signals", rating: 4 },
-			{ name: "Graphics", rating: 2 },
-			{ name: "Linear Algebra", rating: 2 },
-		]
-	},
-	{
-		title: "Product Concepts",
-		entries: [
-			{ name: "Release Cycles", rating: 3 },
-			{ name: "Agile/Scrum", rating: 3 },
-			{ name: "UX Feedback Cycle", rating: 3 },
-			{ name: "Design", rating: 2 }
-		]
-	},{
-		title: "Hardware",
-		entries: [
-			{ name: "Circuit Design & Engineering", rating: 3 },
-			{ name: "Arduino", rating: 3 },
-			{ name: "Thermal and Fluid Mechanics", rating: 3 },
+			{ name: "CAD (Autodesk, Solidworks)", rating: 3 },
+			{ name: "Circuit Analysis", rating: 3 },
+			{ name: "Arduino", rating: 2 },
+			{ name: "Linux on ARM", rating: 2 },
 			{ name: "Bluetooth Low-Energy", rating: 2 }
 		]
-	},
+	},{
+		title: "Software Domain",
+		entries: [
+			{ name: "Full-Stack Web Development", rating: 5 },
+			{ name: "JavaScript", rating: 5 },
+			{ name: "React-Native", rating: 4},
+			{ name: "Relational Databases", rating: 4 },
+			{ name: "DevOps / Infrastructure", rating: 4 }
+		]
+	},{
+		title: "Operations",
+		entries: [
+			{ name: "Software Release Management", rating: 4 },
+			{ name: "UX/UI Design", rating: 3 },
+			{ name: "Technical Hiring", rating: 3 },
+		]
+	}
 ];
 
 
 function initSkills(container){
 	for( var i in skillSets ){
 		var skillSet = skillSets[i];
-		
+
 		var columnDiv = $("<div/>",{
 			"class":"column"
 		}).appendTo(container);
-		
+
 		$("<h3/>",{
 			text: skillSet.title
 		}).appendTo(columnDiv);
-		
+
 		for( var j in skillSet.entries ){
 			var skill = skillSet.entries[j];
-			
+
 			var entryDiv = $("<div/>",{
 				"class":"entry"
 			}).appendTo(columnDiv);
-			
+
 			var ratingDiv = $("<div/>",{
 				"class": "rating"
 			}).appendTo(entryDiv);
-			
+
 			for( var k = 0; k < skill.rating; k++ ){
 				$("<div/>",{
 					"class":"bar"
 				}).appendTo(ratingDiv);
 			}
-			
+
 			$("<div/>",{
 				"class": "name",
 				"text": skill.name
